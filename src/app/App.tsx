c31204e6abb8b9ad2783751f3568c6b039cee458
unknown <dmshastel@gmail.com>
2026-03-03 14:39:22 +0300

    feat: The ReusebleCard component is created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,22 @@
 import { View } from 'react-native';
-import { ReusableInput } from '@/components/ui/input/ReusableInput';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RecipeCard } from '@/components/ui/card/ReusebleCard';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View>
-        <ReusableInput />
+        <RecipeCard
+          recipeImage={
+            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700,636'
+          }
+          userName={'Test User'}
+          description={'Test Description'}
+          userAvatar={
+            'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211471.png'
+          }
+          initialLiked={true}
+        />
       </View>
     </SafeAreaProvider>
   );
