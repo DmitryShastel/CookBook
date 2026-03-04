e855beae3bb2c7424cba07bca3483b9e8ebd2485
unknown <dmshastel@gmail.com>
2026-03-04 08:57:38 +0300

    feat: The LoginValidation schema is created

---
+++ src/screens/auth/login/LoginScreen.tsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
+// import logo from '../../images/logo2.png';
+// import Icon from 'react-native-vector-icons/Ionicons';
+// import { useNavigation } from '@react-navigation/native';
+// import { Formik } from 'formik';
+// import * as yup from 'yup';
+// import { useAuth } from '../auth/authContext';
