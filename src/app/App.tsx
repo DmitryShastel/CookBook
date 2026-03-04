401dd96b64d2e3e0320943226c3dca724e997d4f
unknown <dmshastel@gmail.com>
2026-03-04 09:28:51 +0300

    feat: The LoginScreen is created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,12 @@
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ReusableCard } from '@/components/ui/card/ReusebleCard';
+import { LoginScreen } from '@/screens/auth/login/LoginScreen';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View>
-        <ReusableCard
-          recipeImage={
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700,636'
-          }
-          userName={'Test User'}
-          description={'Test Description'}
-          userAvatar={
-            'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211471.png'
-          }
-          initialLiked={true}
-        />
+        <LoginScreen />
       </View>
     </SafeAreaProvider>
   );
--- src/screens/auth/login/LoginScreen.tsx
+++ src/screens/auth/login/LoginScreen.tsx
@@ -1,8 +1,150 @@
-// import React from 'react';
-// import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
-// import logo from '../../images/logo2.png';
-// import Icon from 'react-native-vector-icons/Ionicons';
-// import { useNavigation } from '@react-navigation/native';
-// import { Formik } from 'formik';
-// import * as yup from 'yup';
-// import { useAuth } from '../auth/authContext';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Formik } from 'formik';
+import { loginValidationSchema } from '@/features/auth/model/lib/LoginValidation';
+
+export const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} />
+      <Text style={styles.title}>Login</Text>
+      <Formik
+        validationSchema={loginValidationSchema}
+        initialValues={{ email: '', password: '' }}
+        onSubmit={() => {}}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+          isValid,
+        }) => (
+          <>
+            <View style={styles.inputContainer}>
+              <Icon name="mail-outline" size={25} style={styles.icon} />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                keyboardType="email-address"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+            </View>
+            {errors.email && touched.email && (
+              <Text style={styles.errorText}>{errors.email}</Text>
+            )}
+            <View style={styles.inputContainer}>
+              <Icon name="lock-closed-outline" size={25} style={styles.icon} />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+            </View>
+            {errors.password && touched.password && (
+              <Text style={styles.errorText}>{errors.password}</Text>
+            )}
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.forgotPassword}>Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSubmit}
+              disabled={!isValid}
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.signUp}>
+                Dont have an account?{' '}
+                <Text style={styles.signUpLink}>Sign Up</Text>
+              </Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#1E90FF',
+    borderRadius: 8,
+    height: 50,
+    justifyContent: 'center',
+    marginBottom: 20,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  errorText: {
+    alignSelf: 'flex-start',
+    color: 'red',
+    marginBottom: 10,
+  },
+  forgotPassword: {
+    alignSelf: 'flex-end',
+    color: '#000',
+    marginBottom: 20,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: '100%',
+  },
+  inputContainer: {
+    alignItems: 'center',
+    backgroundColor: '#f1f1f1',
+    borderRadius: 8,
+    flexDirection: 'row',
+    height: 50,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  logo: {
+    height: 200,
+    marginBottom: 20,
+    resizeMode: 'contain',
+    width: 200,
+  },
+  signUp: {
+    color: '#000',
+  },
+  signUpLink: {
+    color: '#1E90FF',
+  },
+  title: {
+    color: 'black',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+});
