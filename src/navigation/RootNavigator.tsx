2d450fecb861e5050edeb38016c8d3eaa1b05215
unknown <dmshastel@gmail.com>
2026-03-05 16:01:36 +0300

    feat: The RootStack is updated

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,14 +1,23 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootPage } from '@/screens/rootPage/RootPage';
-import { UserProfileScreen } from '@/screens/userProfile/UserProfileScreen';
+import { MainTabNavigator } from '@/navigation/MainTabNavigator';
+import { LoginScreen } from '@/screens/auth/login/LoginScreen';
+import { SignUpScreen } from '@/screens/auth/signUp/SignUpScreen';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  MainTabs: undefined;
+  Login: undefined;
+  SignUp: undefined;
+};
 
-export function RootStack() {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const RootStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={RootPage} />
-      {/*<Stack.Screen name="profile" component={UserProfileScreen} />*/}
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="MainTabs" component={MainTabNavigator} />
+
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
   );
-}
+};
