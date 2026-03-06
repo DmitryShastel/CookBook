a9a11782ed81d94a032e73f5dbe67a35e3e6ba08
unknown <dmshastel@gmail.com>
2026-03-06 11:31:34 +0300

    refactor: The App is updated - commented out code is removed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootScreen } from '@/screens/rootPage/RootPage';
-import { UserProfileScreen } from '@/screens/userProfile/UserProfileScreen';
 import { Button } from '@/components/ui/button/Button';
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <SafeAreaProvider>
       <View style={{ flex: 1, backgroundColor: '#fff' }}>
         <RootScreen>
-          {/*<UserProfileScreen />*/}
           <Button />
         </RootScreen>
       </View>
