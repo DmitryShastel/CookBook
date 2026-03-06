514b35a154c404b1a6d16a0e032f7794f691ca78
unknown <dmshastel@gmail.com>
2026-03-06 11:05:38 +0300

    feat: The MainTabNavigator is updated

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -2,36 +2,39 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RootPage } from '@/screens/rootPage/RootPage';
 import { SettingsScreen } from '@/screens/settings/SettingsScreen';
 import { UserProfileScreen } from '@/screens/userProfile/UserProfileScreen';
+import { View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export const MainTabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      tabBar={({ state, navigation }) => {
-        const activeTab = state.routes[state.index].name.toLowerCase() as
-          | 'home'
-          | 'settings'
-          | 'profile';
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        tabBar={({ state, navigation }) => {
+          const activeTab = state.routes[state.index].name.toLowerCase() as
+            | 'home'
+            | 'settings'
+            | 'profile';
 
-        return (
-          <RootPage
-            activeTab={activeTab}
-            onTabPress={(tab) => {
-              navigation.navigate(tab.charAt(0).toUpperCase() + tab.slice(1));
-            }}
-            showBackButton={false}
-            hideTabs={false}
-          ></RootPage>
-        );
-      }}
-    >
-      <Tab.Screen name="Home" component={() => {}} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="Profile" component={UserProfileScreen} />
-    </Tab.Navigator>
+          return (
+            <RootPage
+              activeTab={activeTab}
+              onTabPress={(tab) => {
+                navigation.navigate(tab.charAt(0).toUpperCase() + tab.slice(1));
+              }}
+              showBackButton={false}
+              hideTabs={false}
+            ></RootPage>
+          );
+        }}
+      >
+        <Tab.Screen name="Home" component={() => {}} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={UserProfileScreen} />
+      </Tab.Navigator>
+    </View>
   );
 };
