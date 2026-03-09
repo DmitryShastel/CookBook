841b47d3b5b820a475f83bbed159adbe7c6be891
unknown <dmshastel@gmail.com>
2026-03-09 10:30:52 +0300

    feat: The LoginScreen/SignUpScreen are changed

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,8 +1,54 @@
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
+// import { Button, Icon } from 'react-native-elements';
+// import { styles } from '@/screens/home/HomeScreen.styles';
+//
+// export const HomeScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.content}>
+//         <Icon name="account-circle" size={100} color="#2089dc" />
+//
+//         <Text style={styles.message}>
+//           Please sign in or create an account to start using the app
+//         </Text>
+//       </View>
+//
+//       <View style={styles.buttonContainer}>
+//         <Button
+//           title="Sign In"
+//           onPress={() => {}}
+//           buttonStyle={styles.button}
+//           titleStyle={styles.buttonTitle}
+//         />
+//
+//         <Button
+//           title="Sign Up"
+//           onPress={() => {}}
+//           buttonStyle={styles.button}
+//           titleStyle={styles.buttonTitle}
+//         />
+//       </View>
+//     </View>
+//   );
+// };
+
+import { Text, View } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
 import { styles } from '@/screens/home/HomeScreen.styles';
+import { useNavigation } from '@react-navigation/native';
+import { HomeScreenNavigationProp } from '@/screens/rootPage/type';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
+  const handleSignIn = () => {
+    navigation.navigate('Login');
+  };
+
+  const handleSignUp = () => {
+    navigation.navigate('SignUp');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
@@ -16,14 +62,14 @@ export const HomeScreen = () => {
       <View style={styles.buttonContainer}>
         <Button
           title="Sign In"
-          onPress={() => {}}
+          onPress={handleSignIn}
           buttonStyle={styles.button}
           titleStyle={styles.buttonTitle}
         />
 
         <Button
           title="Sign Up"
-          onPress={() => {}}
+          onPress={handleSignUp}
           buttonStyle={styles.button}
           titleStyle={styles.buttonTitle}
         />
