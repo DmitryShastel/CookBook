00fcfd624d21ef6d1bdfe78257fa70933ab78ce9
unknown <dmshastel@gmail.com>
2026-03-09 12:25:05 +0300

    fix: The App styles are removed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,13 @@
-import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { styles } from '@/app/App.styles';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStack } from '@/navigation/RootStack';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </View>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
