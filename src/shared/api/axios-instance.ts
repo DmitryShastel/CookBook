fa6e9ef747317333e2531440a63e78b7e224de8d
unknown <dmshastel@gmail.com>
2026-03-10 12:17:14 +0300

    feat: The function getCategories is created

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -2,7 +2,19 @@ import axios from 'axios';
 
 export const BaseUrl = 'www.themealdb.com/api/json/v1/1/';
 
+export interface Category {
+  strCategoryDescription: string;
+  strCategoryThumb: string;
+  strCategory: string;
+  idCategory: string;
+}
+
 export const apiClient = axios.create({
   baseURL: BaseUrl,
   headers: { 'Content-Type': 'application/json' },
 });
+
+export const getCategories = async () => {
+  const { data } = await apiClient.get('/categories.php');
+  return data.categories as Category[];
+};
