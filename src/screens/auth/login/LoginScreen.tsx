71a53c75a36118fb3e459831a4cdfbf4f1ac31e9
unknown <dmshastel@gmail.com>
2026-03-12 14:02:06 +0300

    feat: The LoginForm for demo is created

--- src/screens/auth/login/LoginScreen.tsx
+++ src/screens/auth/login/LoginScreen.tsx
@@ -4,39 +4,39 @@ import { styles } from '@/screens/auth/login/LoginScreen.styles';
 import { useNavigation } from '@react-navigation/native';
 import { LoginForm } from '@/features/auth/ui/loginForm/LoginForm';
 
-// interface LoginScreenProps {
-//   onLogin?: () => void;
-// }
+interface LoginScreenProps {
+  onLogin?: () => void;
+}
 
-// export const LoginScreen = ({ onLogin }: LoginScreenProps) => {
-export const LoginScreen = () => {
-  // const navigation = useNavigation();
+export const LoginScreen = ({ onLogin }: LoginScreenProps) => {
+  // export const LoginScreen = () => {
+  const navigation = useNavigation();
 
-  // const handleLogin = () => {
-  //   onLogin?.();
-  // };
+  const handleLogin = () => {
+    onLogin?.();
+  };
   //don't remove
-  return (
-    <View style={styles.container}>
-      <Image containerStyle={styles.logoContainer} resizeMode="contain" />
-      <Text style={styles.title} h1>
-        Login
-      </Text>
-
-      <LoginForm onForgotPassword={() => {}} />
-    </View>
-  );
-
-  //for fake login
   // return (
   //   <View style={styles.container}>
-  //     <Text>Login Screen</Text>
-  //     <TouchableOpacity onPress={handleLogin}>
-  //       <Text>Login (demo)</Text>
-  //     </TouchableOpacity>
-  //     <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-  //       <Text>Go to Sign Up</Text>
-  //     </TouchableOpacity>
+  //     <Image containerStyle={styles.logoContainer} resizeMode="contain" />
+  //     <Text style={styles.title} h1>
+  //       Login
+  //     </Text>
+  //
+  //     <LoginForm onForgotPassword={() => {}} />
   //   </View>
   // );
+
+  //for fake login
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login (demo)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+        <Text>Go to Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
