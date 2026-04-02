2495a7715997b82a9cf83ce4fc7b6f6e4925c0d7
unknown <dmshastel@gmail.com>
2026-04-02 15:32:11 +0300

    feat/PushNotifications configuration is added

---
+++ src/shared/api/firebase/webPush.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { firebaseConfig } from '@firebase-config';
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export async function requestWebPushToken() {
+  try {
+    const token = await getToken(messaging, {
+      vapidKey:
+        'BFz75opVTfUK0GGqSsDrjg6PSEkxnGXnmNgih4wezpSBHZOjejIqQ6OuT9B2805MlR0u9OtEf-v_Aj4jqbgX188',
+    });
+
+    console.log('token:', token);
+    return token;
+  } catch (err) {
+    console.error('Error:', err);
+    return null;
+  }
+}
+
+export function subscribeToForegroundMessages(
+  callback: (payload: unknown) => void,
+) {
+  onMessage(messaging, (payload) => {
+    callback(payload);
+  });
+}
